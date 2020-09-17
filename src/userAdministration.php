26b1a669fc21223cdbd63b1572745ec7f3ad4cce
Malte <malte.luttermann@student.uni-luebeck.de>
2020-09-17 19:40:44 +0200

    Finished user administration

--- src/userAdministration.php
+++ src/userAdministration.php
@@ -11,6 +11,8 @@ switch ($argv[1]) {
 		addUser();
 		break;
 	case 'edit':
+		$userId = readline('ID: ');
+		editUser($userId);
 		break;
 	case 'delete':
 		$userId = readline('ID: ');
@@ -39,16 +41,78 @@ function addUser() : void {
 	do {
 		$userData['userName'] = readline('Username: ');
 		$isAvailable = $jsonReader->searchValue([], $userData['userName'], 'userName') === false;
+		$isValidName = Utilities::isValidUserName($userData['userName']);
 		
 		if (!$isAvailable) {
 			echo 'Username already taken.' . PHP_EOL;
+		} else if (!$isValidName) {
+			echo 'Invalid username.' . PHP_EOL;
 		}
-	} while (!Utilities::isValidUserName($userData['userName']) || !$isAvailable);
+	} while (!$isValidName || !$isAvailable);
 
-	
+	$userData['pwd'] = readPwd($userData['salt']);
+	$userData['userId'] = createId();
+
+	$jsonReader->setValue([null], $userData);
+	echo "Created user {$userData['userName']} successfully.";
 }
 
-function editUser() : void {}
+/**
+ * Reads a password from the console and returns a hash of it (including salt).
+ * @param string $salt A salt for the password.
+ * @return string A hash of password and salt.
+ */
+function readPwd(string $salt) : string {
+	$pwd = '';
+
+	system('stty -echo');
+	do {
+		echo 'New password: ';
+		$pwd = rtrim(fgets(STDIN), PHP_EOL);
+		echo 'Password (again): ';
+		$pwdAgain = rtrim(fgets(STDIN), PHP_EOL);
+
+		if ($pwd !== $pwdAgain) {
+			echo 'Passwords are not matching.' . PHP_EOL;
+		}
+	} while ($pwd !== $pwdAgain);
+	system('stty echo');
+
+	return hash('sha512', $pwd . $salt);
+}
+
+/**
+ * Creates an identifier for a new user.
+ * @return int The new id.
+ */
+function createId() : int {
+	global $jsonReader;
+
+	$newId = -1;
+
+	foreach ($jsonReader->getArray() as $userId => $userData) {
+		$newId = $userId;
+	}
+
+	return $newId + 1;
+}
+
+/**
+ * Changes the password of a user, given by id.
+ * @param string $userId The id of the user for whom the password should be changed.
+ */
+function editUser(string $userId) : void {
+	global $jsonReader;
+
+	$newSalt = Utilities::randomString(50, Utilities::DEFAULT_ALPHABET);
+	$newPwd = readPwd($newSalt);
+
+	if (readline("Really change the password for user with id {$userId}? (y/n)") === 'y') {
+		$jsonReader->setValue([$userId, 'pwd'], $newPwd);
+		$jsonReader->setValue([$userId, 'salt'], $newSalt);
+		echo 'Changed password successfully.';
+	}
+}
 
 /**
  * Deletes a user, given by id.
