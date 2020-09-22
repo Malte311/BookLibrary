648df9ed8f877604db2ab62bbe567a34e1443a26
Malte <malte.luttermann@student.uni-luebeck.de>
2020-09-22 07:56:23 +0200

    Impressum & Datenschutz

--- src/php/View.php
+++ src/php/View.php
@@ -16,6 +16,13 @@ class View {
 	 * @param Template $template The template object to manipulate html content.
 	 */
 	public function __construct(Template $template) {
+		$baseUrl = !empty($_ENV['BASEURL']) ? $_ENV['BASEURL'] : 'http://localhost:8000';
+		$impressum = !empty($_ENV['IMPRESSUMURL']) ? $_ENV['IMPRESSUMURL'] : '';
+		$datenschutz = !empty($_ENV['DATENSCHUTZURL']) ? $_ENV['DATENSCHUTZURL'] : '';
+
+		$template->addReplacement('%%YEAR%%', date('Y'))->addReplacement('%%BASEURL%%', $baseUrl)
+			->addReplacement('%%IMP%%', $impressum)->addReplacement('%%DS%%', $datenschutz);
+		
 		$this->template = $template;
 	}
 
